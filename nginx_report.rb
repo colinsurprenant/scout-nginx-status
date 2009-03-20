f914d3608dcd2ae1987fc1a4b7d0e52d5d18dc6d
Colin Surprenant <colin@colin-surprenants-macbook-pro-15.local>
2009-03-19 23:15:02 -0400

    refactored parsing

--- nginx_report.rb
+++ nginx_report.rb
@@ -2,25 +2,33 @@ require 'open-uri'
 
 class NginxReport < Scout::Plugin
 
-  def build_report	
-	url = option(:url) || 'http://127.0.0.1/nginx_status'
-
-	total, requests, reading, writing, waiting = nil
-
-	open(url) {|f|
-	  f.each_line do |line|
-		total = $1 if line =~ /^Active connections:\s+(\d+)/
-		if line =~ /^Reading:\s+(\d+).*Writing:\s+(\d+).*Waiting:\s+(\d+)/
-		  reading = $1
-		  writing = $2
-		  waiting = $3
-		end
-		requests = $3 if line =~ /^\s+(\d+)\s+(\d+)\s+(\d+)/  
-	  end
-	}
-	
-	add_report(:total => total, :reading => reading, :writing => writing, :waiting => waiting, :requests => requests)
+  def build_report	  
+    add_report(parse_nginx_status)
   end
+  
+  def parse_nginx_status
+    parsers = [
+      lambda { |line, status| status[:total] = $1 if line =~ /^Active connections:\s+(\d+)/ },
+      lambda { |line, status| nil },
+      lambda { |line, status| status[:requests] = $3 if line =~ /^\s+(\d+)\s+(\d+)\s+(\d+)/ },
+      lambda do |line, status| 
+        if line =~ /^Reading:\s+(\d+).*Writing:\s+(\d+).*Waiting:\s+(\d+)/
+          status[:reading] = $1; status[:writing] = $2; status[:waiting] = $3
+        end
+      end  
+    ]
+    url = option(:url) || 'http://127.0.0.1/nginx_status'
+    result = {}
+
+    open(url) do |f|
+      f.each_line do |line|
+        parsers.shift.call(line, result)
+      end
+    end
+    
+    return result
+  end
+  
 end
 
 
